2ff56571240b6c1d344558a53df517e5019f0642
PtitJao <jordan.lacroix41@gmail.com>
2018-05-22 19:15:00 +0200

    Changed applied to modal window

--- src/Util/ModalWindows/ModalInfoWindow.java
+++ src/Util/ModalWindows/ModalInfoWindow.java
@@ -1,10 +1,12 @@
 package Util.ModalWindows;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 
 public class ModalInfoWindow extends ModalWindow {
     public ModalInfoWindow(String title, String message) {
@@ -12,11 +14,16 @@ public class ModalInfoWindow extends ModalWindow {
 
         VBox box = new VBox();
         box.setAlignment(Pos.CENTER);
+        box.setPadding(new Insets(10, 100, 10, 100));
+        box.setSpacing(10);
+
+        Label label = new Label(message);
+        label.setTextAlignment(TextAlignment.CENTER);
 
         Button button = new Button("OK");
         button.setOnAction(e -> stage.close());
 
-        box.getChildren().addAll(new Label(message), button);
+        box.getChildren().addAll(label, button);
 
         Scene scene = new Scene(box);
         stage.setScene(scene);

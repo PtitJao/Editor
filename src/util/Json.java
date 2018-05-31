17928e18127929f6590cbbff79679982b609c3b2
M-Islam <natsu.light.im7@gmail.com>
2018-05-31 02:27:30 +0200

    La classe avec les deux méthodes

--- src/util/Json.java
+++ src/util/Json.java
@@ -1,7 +1,11 @@
 package Util;
 
 import org.json.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -20,6 +24,50 @@ public class Json {
         this.value = value;
         this.list = list;
         this.array = array;
+        this.fileName = fileName;
+    }
+
+    public Json(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public List<Bar> getList() {
+        return list;
+    }
+
+    public Bar[] getArray() {
+        return array;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public void setList(List<Bar> list) {
+        this.list = list;
+    }
+
+    public void setArray(Bar[] array) {
+        this.array = array;
+    }
+
+    public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
@@ -60,6 +108,34 @@ public class Json {
 
     public void readJsonFile()
     {
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader(fileName));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            this.nom = (String) jsonObject.get("nom");
+            Integer value = (Integer) jsonObject.get("value");
+
+            JSONArray list = (JSONArray) jsonObject.get("list");
+            for (int i = 0; i <list.length() ; i++) {
+                this.list.add((Bar) list.get(i));
+            }
+
+            JSONArray array = (JSONArray) jsonObject.get("array");
+            for (int i = 0; i <array.length() ; i++) {
+                this.array[i] = (Bar) array.get(i);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
